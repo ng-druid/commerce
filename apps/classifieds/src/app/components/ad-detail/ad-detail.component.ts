c931476ac37925de9a7f6881a86b0a9f65983eba
Todd Zmijewski <sme.s.key@gmail.com>
2019-12-05 17:42:43 -0500

    Use secure url for displaying media.

--- apps/classifieds/src/app/components/ad-detail/ad-detail.component.ts
+++ apps/classifieds/src/app/components/ad-detail/ad-detail.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AdDetail } from '@classifieds-ui/ads';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'classifieds-ui-ad-detail',
   templateUrl: './ad-detail.component.html',
   styleUrls: ['./ad-detail.component.scss']
 })
-export class AdDetailComponent {
+export class AdDetailComponent implements OnInit {
   @Input()
   ad: AdDetail;
+  mediaBaseUrl: string;
+  ngOnInit() {
+    this.mediaBaseUrl = environment.mediaSettings.endpointUrl;
+  }
 }
