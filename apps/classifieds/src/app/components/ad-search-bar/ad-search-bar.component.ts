f25405b8d25a56519ae90df9bd70c370e9c667dc
Todd Zmijewski <sme.s.key@gmail.com>
2019-12-07 16:00:03 -0500

    Continue styling.

--- apps/classifieds/src/app/components/ad-search-bar/ad-search-bar.component.ts
+++ apps/classifieds/src/app/components/ad-search-bar/ad-search-bar.component.ts
@@ -1,8 +1,6 @@
-import { Component, AfterViewInit, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 import { fromEvent, Subscription } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { User } from 'oidc-client';
 
 import { AuthFacade } from '@classifieds-ui/auth';
 
@@ -11,9 +9,8 @@ import { AuthFacade } from '@classifieds-ui/auth';
   templateUrl: './ad-search-bar.component.html',
   styleUrls: ['./ad-search-bar.component.scss']
 })
-export class AdSearchBarComponent implements OnInit, AfterViewInit {
+export class AdSearchBarComponent implements AfterViewInit {
   searchString = '';
-  user$: Observable<User>;
 
   @Output()
   searchChange = new EventEmitter<string>();
@@ -23,20 +20,10 @@ export class AdSearchBarComponent implements OnInit, AfterViewInit {
 
   private keyupSubscription: Subscription;
 
-  constructor(private authFacade: AuthFacade) {}
-
-  ngOnInit() {
-    this.user$ = this.authFacade.getUser$;
-  }
-
   ngAfterViewInit() {
     this.keyupSubscription = fromEvent(this.searchField.nativeElement, 'keyup').pipe(
       debounceTime(1000)
     ).subscribe(() => this.onSubmit());
-  }
-
-  login() {
-    this.authFacade.login();
   }
 
   onSubmit() {
--- apps/classifieds/src/app/components/app-header/app-header.component.ts
+++ apps/classifieds/src/app/components/app-header/app-header.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'oidc-client';
+
+import { AuthFacade } from '@classifieds-ui/auth';
 
 @Component({
   selector: 'classifieds-ui-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss']
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit {
+  user$: Observable<User>;
+  constructor(private authFacade: AuthFacade) {}
+  ngOnInit() {
+    this.user$ = this.authFacade.getUser$;
+  }
+  login() {
+    this.authFacade.login();
+  }
 }
