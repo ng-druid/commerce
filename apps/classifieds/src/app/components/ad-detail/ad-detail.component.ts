551350c10e2d6661791e6b21952a192816dd5a2a
Todd Zmijewski <sme.s.key@gmail.com>
2019-12-07 04:10:25 -0500

    Continue creating layout.

--- apps/classifieds/src/app/components/ad-detail/ad-detail.component.ts
+++ apps/classifieds/src/app/components/ad-detail/ad-detail.component.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AdDetail } from '@classifieds-ui/ads';
 import { environment } from '../../../environments/environment';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'classifieds-ui-ad-detail',
   templateUrl: './ad-detail.component.html',
   styleUrls: ['./ad-detail.component.scss']
 })
-export class AdDetailComponent implements OnInit {
+export class AdDetailComponent implements OnInit, OnChanges {
   @Input()
   ad: AdDetail;
   mediaBaseUrl: string;
+  displayOverlay = true;
+  constructor(private route: ActivatedRoute) { }
   ngOnInit() {
     this.mediaBaseUrl = environment.mediaSettings.endpointUrl;
+    this.route.paramMap.pipe(
+      map(p => p.get('adId')),
+      filter(adId => typeof(adId) === 'string')
+    ).subscribe((adId: string) => {
+      this.displayOverlay = true;
+    });
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    this.displayOverlay = false;
   }
 }
