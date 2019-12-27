dc076cf4b6346c6bc58d99a5e4383eb21d590c95
Todd Zmijewski <sme.s.key@gmail.com>
2019-12-27 13:19:34 -0500

    Persist human name to machine name temporarily so that unsaved term values can be persisted between saves.

--- libs/taxonomy/src/lib/taxonomy.module.ts
+++ libs/taxonomy/src/lib/taxonomy.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '@classifieds-ui/material';
 import { TaxonomySelectorComponent } from './components/taxonomy-selector/taxonomy-selector.component';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, HttpClientModule],
+  imports: [CommonModule, MaterialModule, HttpClientModule, FormsModule],
   declarations: [TaxonomySelectorComponent],
   exports: [TaxonomySelectorComponent]
 })
