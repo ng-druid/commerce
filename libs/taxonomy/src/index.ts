b55c87f11055ab611e73f5ea19c189a365e47efd
Todd Zmijewski <sme.s.key@gmail.com>
2019-12-31 19:55:52 -0500

    Remove taxonomy service.

--- libs/taxonomy/src/index.ts
+++ libs/taxonomy/src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/taxonomy.module';
 export * from './lib/models/taxonomy.models';
 export * from './lib/taxonomy.tokens';
-export * from './lib/services/taxonomy.service';
 export * from './lib/components/vocabulary-edit/vocabulary-edit.component';
 export * from './lib/components/vocabulary-create/vocabulary-create.component';
 export * from './lib/services/vocabulary.service';
