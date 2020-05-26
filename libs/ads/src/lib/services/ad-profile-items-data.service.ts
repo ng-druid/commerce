a1ccda29c432b02cd4903cac5212e84f4204524f
Todd Zmijewski <sme.s.key@gmail.com>
2020-05-26 14:34:54 -0400

    Fix gallery height and swap out profile api with go one.

--- libs/ads/src/lib/services/ad-profile-items-data.service.ts
+++ libs/ads/src/lib/services/ad-profile-items-data.service.ts
@@ -8,8 +8,9 @@ import { AdProfileItem } from '../models/ads.models';
   providedIn: 'root'
 })
 export class AdProfileItemsDataService extends DefaultDataService<AdProfileItem> {
+  private goApi = 'https://p1vgub4jtb.execute-api.us-east-1.amazonaws.com';
   constructor(config: DefaultDataServiceConfig, http: HttpClient, httpUrlGenerator: HttpUrlGenerator, logger: Logger) {
     super('AdProfileItem', http, httpUrlGenerator, config);
-    this.entityUrl = this.entitiesUrl = `${config.root}/profiles/profilenavitems/`;
+    this.entityUrl = this.entitiesUrl = `${this.goApi}/profiles/profilenavitems`;
   }
 }
